db240bc633b5d8f5d300f8e6fbd93b0c276f2123
talyamm <cfroggg@mail.ru>
2024-05-28 11:15:18 +0300

    fixed left menu

--- interface/routes.py
+++ interface/routes.py
@@ -4,3 +4,11 @@ from flask import render_template, send_file, request
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/page1')
+def page1():
+    return render_template("page1.html")
+
+@app.route('/page2')
+def page2():
+    return render_template("page2.html")
