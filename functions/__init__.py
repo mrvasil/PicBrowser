7399cce851c04246eca640f0474df8659e8fff53
talyamm <cfroggg@mail.ru>
2024-05-30 10:53:26 +0300

    metadata parsing

--- functions/__init__.py
+++ functions/__init__.py
@@ -2,17 +2,22 @@ import uuid
 import os
 import exiftool
 
+import exiftool
+
 def get_metadata(image_path):
     try:
-        with exiftool.ExifToolHelper() as et:
-            metadata = ""
-            data=et.get_metadata(image_path)[0]
-            for d in data:
-                metadata += d+":"+str(data[d])+"\n"
-    except:
-        metadata = "exiftool doesn't work your device"
-    return metadata
-
+        with exiftool.ExifTool() as et:
+            metadata = et.execute_json('-G', '-j', image_path)
+            specific_data = "\n".join([
+                f"Model: {metadata[0].get('EXIF:Model', 'N/A')}",
+                f"ExposureTime: {metadata[0].get('EXIF:ExposureTime', 'N/A')}",
+                f"FNumber: {metadata[0].get('EXIF:FNumber', 'N/A')}",
+                f"ISO: {metadata[0].get('EXIF:ISO', 'N/A')}"
+            ])
+    except Exception as e:
+        specific_data = f"ExifTool error: {str(e)}"
+    return specific_data
+    
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in {'zip'}
 
