b513741475ca343a61e6fb000337306da168b537
AlexeykaVas <alexeyka.v@yandex.ru>
2024-06-06 12:50:45 +0300

    change db structure

--- functions/__init__.py
+++ functions/__init__.py
@@ -13,39 +13,44 @@ class Database:
     def close_connection(conn):
         conn.close()
 
-def init_db(user_code):
+def init_db():
     conn = Database.get_connection()
     c = conn.cursor()
-    c.execute(f'''CREATE TABLE IF NOT EXISTS "{user_code}" (
-                  id INTEGER PRIMARY KEY AUTOINCREMENT,
-                  filename TEXT NOT NULL,
-                  visible BOOLEAN NOT NULL DEFAULT 1,
-                  order_index INTEGER DEFAULT 0,
-                  UNIQUE(filename)
-                )''')
+    c.execute('''CREATE TABLE IF NOT EXISTS images (
+                      id INTEGER PRIMARY KEY AUTOINCREMENT,
+                      user_code TEXT NOT NULL,
+                      filename TEXT NOT NULL,
+                      visible BOOLEAN NOT NULL DEFAULT 1,
+                      order_index INTEGER DEFAULT 0,
+                      UNIQUE(user_code, filename)
+                    )''')
+    c.execute('''CREATE TABLE IF NOT EXISTS user_codes (
+                    user_code TEXT PRIMARY KEY
+            )''')
     conn.commit()
     Database.close_connection(conn)
+
 
 def add_image_to_db(user_code, filename):
     conn = Database.get_connection()
     c = conn.cursor()
 
-    c.execute(f'''INSERT INTO "{user_code}" (filename, visible) VALUES (?, 1)
-                  ON CONFLICT(filename) DO UPDATE SET visible = 1''', (filename,))
+    c.execute('''INSERT INTO images (user_code, filename, visible) VALUES (?, ?, 1)
+                  ON CONFLICT(user_code, filename) DO UPDATE SET visible = 1''', (user_code, filename))
     conn.commit()
     Database.close_connection(conn)
 
 def remove_image_from_db(user_code, filename):
     conn = Database.get_connection()
     c = conn.cursor()
-    c.execute(f'''UPDATE "{user_code}" SET visible = 0 WHERE filename = ?''', (filename,))
+    c.execute('''UPDATE images SET visible = 0 WHERE user_code = ? AND filename = ?''', (user_code, filename))
     conn.commit()
     Database.close_connection(conn)
 
 def get_visible_images(user_code):
     conn = Database.get_connection()
     c = conn.cursor()
-    c.execute(f'''SELECT filename FROM "{user_code}" WHERE visible = 1 ORDER BY order_index, id''')
+    c.execute('''SELECT filename FROM images WHERE user_code = ? AND visible = 1 ORDER BY order_index, id''', (user_code,))
     images = [row[0] for row in c.fetchall()]
     Database.close_connection(conn)
     return images
@@ -54,7 +59,7 @@ def update_image_order(user_code, filename, new_index):
     conn = Database.get_connection()
     c = conn.cursor()
 
-    c.execute(f'''SELECT filename, order_index FROM "{user_code}" WHERE visible = 1 ORDER BY order_index, id''')
+    c.execute('''SELECT filename, order_index FROM images WHERE user_code = ? AND visible = 1 ORDER BY order_index, id''', (user_code,))
     files = c.fetchall()
 
     updated_files = []
@@ -66,7 +71,7 @@ def update_image_order(user_code, filename, new_index):
     updated_files.insert(new_index, (filename, 0))
 
     for index, file in enumerate(updated_files):
-        c.execute(f'''UPDATE "{user_code}" SET order_index = ? WHERE filename = ?''', (index, file[0]))
+        c.execute('''UPDATE images SET order_index = ? WHERE user_code = ? AND filename = ?''', (index, user_code, file[0]))
     conn.commit()
     Database.close_connection(conn)
 
@@ -75,7 +80,7 @@ def remove_all_images_from_db(user_code):
     conn = Database.get_connection()
     c = conn.cursor()
     try:
-        c.execute(f'''DROP TABLE IF EXISTS "{user_code}"''')
+        c.execute('''DELETE FROM images WHERE user_code = ?''', (user_code,))
         conn.commit()
     finally:
         Database.close_connection(conn)
@@ -87,16 +92,17 @@ def get_user_code(request):
     c = conn.cursor()
     
     if user_code:
-        c.execute("SELECT name FROM sqlite_master WHERE type='table' AND name=?", (user_code,))
+        c.execute("SELECT 1 FROM user_codes WHERE user_code = ?", (user_code,))
         exists = c.fetchone()
     else:
         exists = False
 
     if not exists:
         user_code = str(uuid.uuid4())
-        init_db(user_code)
+        c.execute("INSERT INTO user_codes (user_code) VALUES (?)", (user_code,))
         os.makedirs(os.path.join('uploads', user_code), exist_ok=True)
     
+    conn.commit()
     Database.close_connection(conn)
     return user_code
 
--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 from interface import app
+from functions import init_db
 
 if __name__ == '__main__':
+    init_db()
     app.run(host="0.0.0.0", port=20191, debug=True)
