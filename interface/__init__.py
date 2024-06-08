c0532347bdf6f5285146051a9c6377333c5527ac
AlexeykaVas <alexeyka.v@yandex.ru>
2024-06-08 12:28:35 +0300

    small change to get real ip passing through nginx

--- interface/__init__.py
+++ interface/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__)
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_port=1, x_prefix=1)
 
 from interface import routes
